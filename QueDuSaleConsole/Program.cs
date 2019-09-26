b8461cb2175e75572af1a6f984caf46c68558460
Onizuuka <matthieu.david@lmdsio.fr>
2019-09-26 08:25:34 +0200

    Ajoutez des fichiers projet.

---
+++ QueDuSaleConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueDuSaleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ------------------------");
+            Console.WriteLine(" | QUEDUSALE PRONOSTICS |");
+            Console.WriteLine(" ------------------------");
+            Console.Read();
+        }
+    }
+}
